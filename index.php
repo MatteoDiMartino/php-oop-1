3410fc209b8ad7331d8e658130761de1d5ef11c3
MatteoDiMartino <matteodimartino359@gmail.com>
2024-05-16 13:17:38 +0200

    feat: add getInfoFilm function

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
-
 class Movie {
     public $name;
     public $year;
     public $type;
 
+    public function __construct($name, $year, $type) {
+        $this->name = $name;
+        $this->year = $year;
+        $this->type = $type;
+    }
+
+    public function getInfoFilm() {
+        return $this->name . ' (' . $this->year . ') - ' . $this->type;
+    }
 }
 
 
